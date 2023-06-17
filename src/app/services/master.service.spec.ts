82bddc3acf9b3f11ef989674b685c9f1d718cd3e
Carlos Andres <carnorat@gmail.com>
2023-06-17 12:15:46 -0500

    Spies para servicos angular

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -5,21 +5,30 @@ import { FakeValueService } from './fake-value-service';
 fdescribe('MasterService', () => {
 
   it('should return "my value service" from  the real service', () => {
-    const  valueService =  new ValueService();
+    const valueService = new ValueService();
     const masterService = new MasterService(valueService);
     expect(masterService.getValue()).toBe('my value service');
   });
 
   it('should return "other value" from  the fake service', () => {
-    const  fakeValueService =  new FakeValueService();
+    const fakeValueService = new FakeValueService();
     const masterService = new MasterService(fakeValueService as unknown as ValueService);
     expect(masterService.getValue()).toBe('fake value');
   });
 
 
   it('should return "other value" from  the fake object', () => {
-    const fake = { getValue: () => 'fake value'};
+    const fake = { getValue: () => 'fake value' };
     const masterService = new MasterService(fake as ValueService);
     expect(masterService.getValue()).toBe('fake value');
+  });
+
+  it('should call to getValue from ValueService', () => {
+    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
+    valueServiceSpy.getValue.and.returnValue('fake value');
+    const masterService = new MasterService(valueServiceSpy);
+    expect(masterService.getValue()).toBe('fake value')
+    expect(valueServiceSpy.getValue).toHaveBeenCalled();
+    expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
   });
 });
